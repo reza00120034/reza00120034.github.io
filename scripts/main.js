39217450f464dae58c9f0115d165435999958770
reza00120034 <43208144+reza00120034@users.noreply.github.com>
2019-01-02 13:18:22 +0330

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,44 @@
+
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+
+    var mySrc = myImage.getAttribute('src');
+
+    if(mySrc === 'images/Photo0034.jpg') {
+
+      myImage.setAttribute ('src','images/Photo0036.jpg');
+
+    } else {
+
+      myImage.setAttribute ('src','images/Photo0034');
+    }
+}
+
+var myButton = document.querySelector('button');
+
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+
+  var myName = prompt('Please enter your name.');
+
+  localStorage.setItem('name', myName);
+
+  myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+
+  setUserName();
+
+} else {
+
+  var storedName = localStorage.getItem('name');
+
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
